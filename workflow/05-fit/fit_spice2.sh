04e4a5deb607dc93b197c77c1d8e5c132f7addcf
Josh Mitchell <yoshanuikabundi@gmail.com>
2025-09-18 22:43:43 -0700

    Ditch pixi for fit

--- workflow/05-fit/fit_spice2.sh
+++ workflow/05-fit/fit_spice2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=jam-spice2-fit1   ## Name of the job.
+#SBATCH --job-name=spice2-fit1       ## Name of the job.
 #SBATCH -A dmobley_lab_gpu           ## account to charge
 #SBATCH -p gpu                       ## partition name
 #SBATCH -t 3-00:00:00                ## Time limit: 3 days
@@ -9,8 +9,8 @@
 #SBATCH --cpus-per-task=1            ## number of cores the job needs
 #SBATCH --gres=gpu:1                 ## number of GPUs to use
 #SBATCH --mem=16gb                   ## amount of memory needed. 1 cpu is billed per 9GB memory on gpu partition
-#SBATCH --error=slurm_%x_%j-%J.err   ## error log file
-#SBATCH --output=slurm_%x_%j-%J.out  ## output log file
+#SBATCH --error=slurm_%u_%x_%J.err   ## error log file
+#SBATCH --output=slurm_%u_%x_%J.out  ## output log file
 
 set -euf -o pipefail
 
@@ -18,10 +18,16 @@ set -euf -o pipefail
 # cd $dir
 cd /dfs9/dmobley-lab/joshm3/back-to-school-josh/workflow/05-fit/
 
-mkdir spice2-fit1
-pixi run -e cuda mamba env export > spice2-fit1/solved-env.yml
+module load mamba/24.3.0
+module list
 
-pixi run -e cuda python fit.py                                                 \
+# mamba env create -n fit-spice2 -f ../../cuda-env.yml
+# mamba activate -n fit-spice2
+
+mkdir -p spice2-fit1
+mamba run -n fit-spice2 mamba env export > spice2-fit1/solved-env.yml
+
+mamba run -n fit-spice2 python fit.py                                          \
     --tensor-ff-path ../04-parametrize/outputs/tensor_ff.pt                    \
     --tensor-tops-path ../04-parametrize/outputs/smiles_to_topologies.pt       \
     --train-dataset-paths ../02-select-data/datasets/spice2/train              \

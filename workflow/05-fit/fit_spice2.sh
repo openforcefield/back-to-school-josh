a770f2e7bbb3b2ad84a9b5a0ff02b57976de34ba
Josh Mitchell <yoshanuikabundi@gmail.com>
2025-09-18 20:57:54 -0700

    Fix submit script

--- workflow/05-fit/fit_spice2.sh
+++ workflow/05-fit/fit_spice2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=jam-spice2-fit1  ## Name of the job.
-#SBATCH -A dmobley_lab_gpu          ## account to charge 
-#SBATCH -p gpu                      ## partition name
-#SBATCH -t 3-00:00:00               ## Time limit: 3 days
-#SBATCH --nodes=1                   ## (-N) number of nodes to use
-#SBATCH --ntasks=1                  ## (-n) number of tasks to launch
-#SBATCH --cpus-per-task=1           ## number of cores the job needs
-#SBATCH --gres=gpu:1                ## number of GPUs to use
-#SBATCH --mem=16gb                  ## amount of memory needed. 1 cpu is billed per 9GB memory on gpu partition
-#SBATCH --error=slurm-%J.err        ## error log file
-#SBATCH --output=slurm-%J.out       ## output log file
+#SBATCH --job-name=jam-spice2-fit1   ## Name of the job.
+#SBATCH -A dmobley_lab_gpu           ## account to charge
+#SBATCH -p gpu                       ## partition name
+#SBATCH -t 3-00:00:00                ## Time limit: 3 days
+#SBATCH --nodes=1                    ## (-N) number of nodes to use
+#SBATCH --ntasks=1                   ## (-n) number of tasks to launch
+#SBATCH --cpus-per-task=1            ## number of cores the job needs
+#SBATCH --gres=gpu:1                 ## number of GPUs to use
+#SBATCH --mem=16gb                   ## amount of memory needed. 1 cpu is billed per 9GB memory on gpu partition
+#SBATCH --error=slurm_%x_%j-%J.err   ## error log file
+#SBATCH --output=slurm_%x_%j-%J.out  ## output log file
 
 set -euf -o pipefail
 
@@ -19,9 +19,9 @@ set -euf -o pipefail
 cd /dfs9/dmobley-lab/joshm3/back-to-school-josh/workflow/05-fit/
 
 mkdir spice2-fit1
-pixi run --env cuda mamba env export > spice2-fit1/solved-env.yml
+pixi run -e cuda mamba env export > spice2-fit1/solved-env.yml
 
-pixi run --env cuda python fit.py                                              \
+pixi run -e cuda python fit.py                                                 \
     --tensor-ff-path ../04-parametrize/outputs/tensor_ff.pt                    \
     --tensor-tops-path ../04-parametrize/outputs/smiles_to_topologies.pt       \
     --train-dataset-paths ../02-select-data/datasets/spice2/train              \
